f10998b502b1e22f29ac9325a4ab8bc4183e613f
Metin <66448640+metinbor@users.noreply.github.com>
2022-04-08 01:33:18 -0400

    update master.sh

--- master.sh
+++ master.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
 #Steps for Master Node (control plane): 
-#1. Swap disabled. You MUST disable swap in order for the kubelet to work properly.
+
+#Enable iptables Bridged Traffic
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+br_netfilter
+EOF
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sudo sysctl --system
+
+
+
+# Swap disabled. You MUST disable swap in order for the kubelet to work properly.
 #disable swap 
 
 sudo swapoff -a
@@ -10,22 +25,25 @@ sudo swapoff -a
 
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g'/etc/fstab
 
-#Update the apt package index and install packages needed to use the Kubernetes apt repository
+#Install the required packages for Docker
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
 
-#Installing Docker
+
+#Add the Docker GPG key and apt repository
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  
+ #Installing Docker 
 
 sudo apt-get update -y
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
-# Following configurations are recomended in the kubenetes documentation for Docker runtime. Please refer https://kubernetes.io/docs/setup/production-environment/container-runtimes/#docker
 
+#Add the docker daemon configurations to use systemd as the cgroup driver.
 #Configure the Cgroup driver
 
 cat <<EOF | sudo tee /etc/docker/daemon.json
@@ -38,6 +56,8 @@ cat <<EOF | sudo tee /etc/docker/daemon.json
   "storage-driver": "overlay2"
 }
 EOF
+
+#Start and enable the docker service
 
 sudo systemctl enable docker
 sudo systemctl daemon-reload
@@ -55,7 +75,7 @@ sudo apt-get install -y apt-transport-https ca-certificates curl
 #2.Download the Google Cloud public signing key:
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 
-#3.Add the Kubernetes apt repository:
+#3.Add the GPG key and the Kubernetes apt repository:
 
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
@@ -70,8 +90,18 @@ sudo apt-get install -y kubelet kubeadm kubectl
 #for install specific versions 
 #sudo apt-get install -y kubelet=1.20.6-00 kubectl=1.20.6-00 kubeadm=1.20.6-00
 
+you can use the following commands to find the latest versions.
+
+#apt update
+#apt-cache madison kubeadm | tac
+
+
+#Add hold to the packages to prevent upgrades.
 sudo apt-mark hold kubelet kubeadm kubectl
 
+#Initialize Kubeadm On Master Node To Setup Control Plane
+
+#First, set two environment variables. Replace 10.0.0.10 with the IP of your master node.
 
 IPADDR="10.0.0.10"
 
@@ -99,10 +129,16 @@ mkdir -p $HOME/.kube
 #deploy a Pod network to the cluster
 #You must deploy a Container Network Interface (CNI) based Pod network add-on so that your Pods can communicate with each other. Cluster DNS (CoreDNS) will not start up before a network is installed.
 
-#deploy calico
+#Install Calico Network Plugin for Pod Networking
 #Calico is an open source networking and network security solution for containers
+
 curl https://docs.projectcalico.org/manifests/calico.yaml -O
 
 kubectl apply -f calico.yaml
+
+#Setup Kubernetes Metrics Server
+#Kubeadm doesn’t install metrics server components during its initialization. We have to install it separately.
+
+kubectl apply -f https://raw.githubusercontent.com/scriptcamp/kubeadm-scripts/main/manifests/metrics-server.yaml
 
 
